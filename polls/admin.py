ac693c2508966ae2be02da91bf4e1d878a58ef61
n_coder <n@example.com>
2017-03-20 15:42:16 +0530

    Cleaning code with Generic Views and Customizing Amin Panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    list_display = ('question_text', 'pub_date')
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
